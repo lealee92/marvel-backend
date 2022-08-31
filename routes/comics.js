655bd1bdfbc2f1ace9ff8133ad08aff93238df05
Léa LEE <lealee@MacBook-Pro-de-Lea.local>
2022-08-31 16:46:29 +0200

    comics route

--- routes/comics.js
+++ routes/comics.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { Router, response } = require("express");
 const express = require("express");
 const router = express.Router();
 
@@ -8,14 +9,39 @@ const apiKey = process.env.API_SECRET;
 
 router.get("/comics", async (req, res) => {
   try {
+    let limit = 100;
+    let page;
+    if (req.query.page < 1) {
+      page = 1;
+    } else {
+      page = req.query.page;
+    }
+
+    let skip = (page - 1) * limit;
+    let title = "";
+    if (req.query.title) {
+      title = req.query.title;
+    }
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apiKey}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?limit=${limit}&skip=${skip}&title=${title}&apiKey=${apiKey}`
     );
-    res.json(response);
+    res.json(response.data);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
   res.status(200).json({ message: "marvel's comics" });
+});
+
+router.get("comics/:characterId", async (req, res) => {
+  try {
+    const id = req.params.characterId;
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${id}?apiKey=${apiKey}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
